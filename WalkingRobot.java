c0fb7ac4c92aa9d12e086b936ff4d240d5c70e11
abhishek nair <abhishek21nair@gmail.com>
2017-11-04 03:09:24 +0530

    *WalkingRobot: Optimized code for better execution

--- WalkingRobot.java
+++ WalkingRobot.java
@@ -15,40 +15,20 @@ class WalkingRobot{
 		}else if(direction.equals("EAST")) {
 			direct = 1;
 		}		
-		
 		walkCalculations(x,y,direct,walk_string);		
 	}
 	
 
 static void walkCalculations(int x,int y, int direction, char path[])
-{
-	
+{	
 	String directionsting = "";
 	for(int i=0;i<path.length;i++) {		
 		char move = path[i];
 		if (move == 'R') {	
-			direction = ( direction + 1)%4; //clck wise directon
-			  /*if(direction == 3) {
-	    		  direction = 0;
-	    	  }else if(direction == 2) {
-	    		  direction = 3;
-	    	  }else if(direction == 1) {
-	    		  direction = 2;
-	    	  }else if(direction == 0) {
-	    		  direction = 1;
-	    	  }*/
+			direction = ( direction + 1)%4; 			  
 			}		      
 	      else if (move == 'L') {
-	    	  direction = (4 + direction - 1)%4;//clck wise directon
-	    	  /*if(direction == 3) {
-	    		  direction = 2;
-	    	  }else if(direction == 2) {
-	    		  direction = 1;
-	    	  }else if(direction == 1) {
-	    		  direction = 0;
-	    	  }else if(direction == 0) {
-	    		  direction = 3;
-	    	  }	*/    	  
+	    	  direction = (4 + direction - 1)%4;	    	     	  
 	      }   	  
 		 else if (move == 'W')
 	      {
@@ -61,19 +41,8 @@ static void walkCalculations(int x,int y, int direction, char path[])
 	         else if(direction == 3 ) //  West	            
 	         x = x -Integer.parseInt( Character.toString(path[i+1]));
 	      }
-	}
-	
-	if(direction == 2) {
-		directionsting = "SOUTH";
-	}else if(direction == 0) {
-		directionsting = "NORTH";
-	}else if(direction == 3) {
-		directionsting = "WEST";
-	}else if(direction == 1) {
-		directionsting = "EAST";
-	}
-	/*directionsting = direction==2?"SOUTH":direction == 0?"NORTH":direction == 3?"WEST":direction == 1?"EAST":"";*/
-	
+	}	
+	directionsting = direction==2?"SOUTH":direction == 0?"NORTH":direction == 3?"WEST":direction == 1?"EAST":"";	
 	
 	System.out.println(x);
 	System.out.println(y);
